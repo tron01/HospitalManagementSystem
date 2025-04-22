3cf4ac8dd878afd10171b9a7a8104209c31ab8fc
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-22 18:03:53 +0530

    AdminUser added

--- src/main/java/com/Abhijith/HospitalManagementSystem/Config/SecurityConfig.java
+++ src/main/java/com/Abhijith/HospitalManagementSystem/Config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder  passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
     @Bean
---
+++ src/main/java/com/Abhijith/HospitalManagementSystem/Service/AdminUserCreate.java
@@ -0,0 +1,29 @@
+package com.Abhijith.HospitalManagementSystem.Service;
+
+import com.Abhijith.HospitalManagementSystem.Model.User;
+import com.Abhijith.HospitalManagementSystem.Repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminUserCreate {
+
+    @Bean
+    public CommandLineRunner UserAdminCreate(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()) {
+                User user = new User();
+                user.setUsername("admin");
+                user.setPassword(passwordEncoder.encode("admin"));
+                user.setRole("ROLE_ADMIN");
+                userRepository.save(user);
+                System.out.println("✅ Admin user created");
+            } else {
+                System.out.println("⚠️ Admin user already exists");
+            }
+        };
+    }
+}
+

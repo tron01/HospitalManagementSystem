b7c7fd21dcfc74ec9de744acd7f60849376352ee
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-25 01:36:46 +0530

    Patient,Doctor request ,response  DTOs

---
+++ src/main/java/com/Abhijith/HospitalManagementSystem/DTO/PatientRegister.java
@@ -0,0 +1,15 @@
+package com.Abhijith.HospitalManagementSystem.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class PatientRegister {
+    private String username;
+    private String password;
+    private String name;
+    private String address;}
---
+++ src/main/java/com/Abhijith/HospitalManagementSystem/DTO/PatientResponse.java
@@ -0,0 +1,15 @@
+package com.Abhijith.HospitalManagementSystem.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class PatientResponse {
+    private Long id;
+    private String username;
+    private String name;
+    private String address;
+}

2e768930fc790d621cd3e80d5551391201b56d0d
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-26 01:00:51 +0530

    token time 1hr

--- src/main/java/com/Abhijith/HospitalManagementSystem/Controller/UserController.java
+++ src/main/java/com/Abhijith/HospitalManagementSystem/Controller/UserController.java
@@ -1,11 +1,13 @@
 package com.Abhijith.HospitalManagementSystem.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/admin")
 public class UserController {
 
     @GetMapping("/users")
--- src/main/java/com/Abhijith/HospitalManagementSystem/Util/JwtUtil.java
+++ src/main/java/com/Abhijith/HospitalManagementSystem/Util/JwtUtil.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final long EXPIRATION_TIME = 1000*60; //1 hr
+    private final long EXPIRATION_TIME = 1000*60*60; //1 hr
     private final String SECRET = "@2313_here_my_secret_key_@#@!!323525262 ";
     private final SecretKey Key = Keys.hmacShaKeyFor(SECRET.getBytes());
     //Generate Jwt Token

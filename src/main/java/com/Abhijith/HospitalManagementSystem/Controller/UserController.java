f060ed39183d30af5554b0c60fe583c2f2b323e9
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-02 23:30:01 +0530

    Update UserController.java

--- src/main/java/com/Abhijith/HospitalManagementSystem/Controller/UserController.java
+++ src/main/java/com/Abhijith/HospitalManagementSystem/Controller/UserController.java
@@ -1,5 +1,6 @@
 package com.Abhijith.HospitalManagementSystem.Controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+@AllArgsConstructor
 public class UserController {
 
     @GetMapping("/users")

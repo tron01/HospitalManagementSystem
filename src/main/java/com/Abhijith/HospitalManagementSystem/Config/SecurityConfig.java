c16a2655563c0746eca3e4c2aab5563950b81cd2
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-02 22:14:43 +0530

    exceptionHandling ,sessionManagement added

--- src/main/java/com/Abhijith/HospitalManagementSystem/Config/SecurityConfig.java
+++ src/main/java/com/Abhijith/HospitalManagementSystem/Config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.Abhijith.HospitalManagementSystem.Config;
 
+import com.Abhijith.HospitalManagementSystem.ExceptionHandler.CustomAuthenticationEntryPoint;
+import com.Abhijith.HospitalManagementSystem.ExceptionHandler.CustomaccessDeniedHandler;
 import com.Abhijith.HospitalManagementSystem.Filter.JwtFilter;
 import com.Abhijith.HospitalManagementSystem.Service.CustomUserServiceDetails;
 import com.Abhijith.HospitalManagementSystem.Util.JwtUtil;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,28 +15,32 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
     private final CustomUserServiceDetails customUserServiceDetails;
-
-    @Autowired
-    public SecurityConfig(JwtFilter jwtFilter, CustomUserServiceDetails customUserServiceDetails) {
-        this.jwtFilter = jwtFilter;
-        this.customUserServiceDetails = customUserServiceDetails;
-    }
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+    private final CustomaccessDeniedHandler accessDeniedHandler;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                    .exceptionHandling(e->
+                            e.authenticationEntryPoint(authenticationEntryPoint)
+                                    .accessDeniedHandler(accessDeniedHandler))
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(
                         req -> req.requestMatchers("/api/auth/login").permitAll()
                                 .requestMatchers("/api/doctor/register").permitAll()
                                 .requestMatchers("/api/patient/register").permitAll()
@@ -41,8 +48,10 @@ public class SecurityConfig {
                                 .requestMatchers("/api/patient/**").hasRole("PATIENT")
                                 .requestMatchers("/api/doctor/**").hasRole("DOCTOR")
                                 .anyRequest().authenticated())
-
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                                .sessionManagement(s->
+                                        s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .addFilterBefore(jwtFilter
+                                ,UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 

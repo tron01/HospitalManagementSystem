0bd3b34998fe12f220562fd313969f5e6a943147
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-03 20:02:55 +0530

    getAppointmentsForLoggedInPatient

--- src/main/java/com/Abhijith/HospitalManagementSystem/Controller/PatientController.java
+++ src/main/java/com/Abhijith/HospitalManagementSystem/Controller/PatientController.java
@@ -1,42 +1,54 @@
 package com.Abhijith.HospitalManagementSystem.Controller;
 
+import com.Abhijith.HospitalManagementSystem.DTO.AppointmentResponse;
 import com.Abhijith.HospitalManagementSystem.DTO.PatientRegister;
 import com.Abhijith.HospitalManagementSystem.DTO.PatientResponse;
+import com.Abhijith.HospitalManagementSystem.Model.Users;
+import com.Abhijith.HospitalManagementSystem.Service.AppointmentService;
 import com.Abhijith.HospitalManagementSystem.Service.PatientService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/patient")
 @AllArgsConstructor
 public class PatientController {
 
     private final PatientService patientService;
+    private final AppointmentService appointmentService;
 
     @PostMapping("/register")
     public ResponseEntity<PatientResponse> registerDoctor(@RequestBody PatientRegister doctorDTO) {
         PatientResponse savedPatient = patientService.registerPatient(doctorDTO);
         return new ResponseEntity<>(savedPatient, HttpStatus.CREATED);
     }
-    @SecurityRequirement(name = "bearerAuth")
-    @GetMapping("/list")
-    public ResponseEntity<List<PatientResponse>> getPatients() {
-        return ResponseEntity.ok(patientService.getAllPatients());
-    }
-    @SecurityRequirement(name = "bearerAuth")
-    @GetMapping("/test")
-    public String test() {
-        return "patient api reachable";
+
+    @GetMapping("/appointments")
+    public ResponseEntity<List<AppointmentResponse>> getAppointmentsForLoggedInPatient() {
+        Users currentUser = (Users) SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+
+        Long patientId = currentUser.getId();
+        List<AppointmentResponse> appointments = appointmentService.getAppointmentsByPatientId(patientId);
+        return ResponseEntity.ok(appointments);
     }
 
     @SecurityRequirement(name = "bearerAuth")
-    @GetMapping("/{id}")
-    public ResponseEntity<PatientResponse> getPatientById(@PathVariable("id") long id) {
-        return ResponseEntity.ok(patientService.getPatientById(id));
+    @GetMapping("/test")
+    public String test() {
+        Users currentUser = (Users) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = currentUser.getId();
+        String role = currentUser.getRole();
+        log.info("User with id:{} and Role : {} is trying to access patient test", userId,role);
+        return "patient api reachable";
     }
+
 
 }
